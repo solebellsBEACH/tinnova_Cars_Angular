7a221d54f25c0ec1808154023bac5fd59a5eae9c
solebellsBEACH <lucassxxavier@gmail.com>
2022-11-06 13:19:35 -0300

    styling brandContent

---
+++ src/app/pages/home/components/brands-content/brands-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-brands-content',
+  templateUrl: './brands-content.component.html',
+  styleUrls: ['./brands-content.component.scss']
+})
+export class BrandsContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,12 +5,14 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { HomeService } from './home.service';
 import { IsSoldComponent } from './components/is-sold/is-sold.component';
+import { BrandsContentComponent } from './components/brands-content/brands-content.component';
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
     ProductItemComponent,
-    IsSoldComponent
+    IsSoldComponent,
+    BrandsContentComponent
   ],
   imports: [
     CommonModule,
