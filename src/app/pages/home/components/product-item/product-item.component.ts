277ea805bbaa7c48cfb1a035b7337a354e02144f
solebellsBEACH <lucassxxavier@gmail.com>
2022-11-06 09:50:42 -0300

    styling car informations

--- src/app/pages/home/components/product-item/product-item.component.ts
+++ src/app/pages/home/components/product-item/product-item.component.ts
@@ -11,6 +11,7 @@ export class ProductItemComponent implements OnInit {
   @Input() color: string = 'white'
   @Input() image: string = 'https://robociencia.com.br/wp-content/uploads/2021/03/robo.png'
   @Input() price: number = 0
+  @Input() year: number = 2000
   constructor() { }
 
   ngOnInit(): void {
